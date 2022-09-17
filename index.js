8da2e59c3a29c23a1dcd78da3b1eceed76db25a9
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-09-18 01:32:39 +0900

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+const PORT = 6000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+
+const URL = "https://search.rakuten.co.jp/search/mall/keyboard/";
+const data = [];
+
+axios(URL)
+  .then((response) => {
+    const htmlParser = response.data;
+    // console.log(htmlParser);
+
+    const $ = cheerio.load(htmlParser);
+    $(".searchresultitem", htmlParser).each(function () {
+      const title = $(this).find(".title").text();
+      const price = $(this).find(".important").text();
+      data.push({ title, price });
+      console.log(data);
+    });
+  })
+  .catch((error) => console.log(error));
+
+app.listen(PORT, console.log("server running"));
